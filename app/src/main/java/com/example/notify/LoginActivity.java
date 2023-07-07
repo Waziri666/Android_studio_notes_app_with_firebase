aa404de71ea184be6c96fc2a4001be9ccef64f90
Eyram <103005414+edeyram@users.noreply.github.com>
2023-07-07 20:39:23 +0000

    Update LoginActivity.java

--- app/src/main/java/com/example/notify/LoginActivity.java
+++ app/src/main/java/com/example/notify/LoginActivity.java
@@ -2,7 +2,6 @@ package com.example.notify;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -11,7 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
