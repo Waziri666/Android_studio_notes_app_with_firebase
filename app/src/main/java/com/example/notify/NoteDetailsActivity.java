e478cf15bc34c4a658c097072d0139c830c17811
Mikey41 <102548306+Mikey41@users.noreply.github.com>
2023-07-07 18:54:18 +0000

    Update NoteDetailsActivity.java

--- app/src/main/java/com/example/notify/NoteDetailsActivity.java
+++ app/src/main/java/com/example/notify/NoteDetailsActivity.java
@@ -2,7 +2,6 @@ package com.example.notify;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
